f0d7e6b9625a8cd4f649266d5e42c5f3b33ce3be
Edward Miranda <emira035@ucr.edu>
2023-03-18 18:52:04 -0700

    added absolute path so i can load_data.sql

--- project/sql/src/load_data.sql
+++ project/sql/src/load_data.sql
@@ -1,36 +1,36 @@
 COPY Users
-FROM 'users.csv' 
+FROM '/extra/emira035/cs166_project/project/data/users.csv' 
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE users_userID_seq RESTART 101; 
 
 COPY Hotel
-FROM 'hotels.csv'
+FROM '/extra/emira035/cs166_project/project/data/hotels.csv'
 WITH DELIMITER ',' CSV HEADER;   
 
 COPY Rooms
-FROM 'rooms.csv'
+FROM '/extra/emira035/cs166_project/project/data/rooms.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY MaintenanceCompany
-FROM 'company.csv'
+FROM '/extra/emira035/cs166_project/project/data/company.csv'
 WITH DELIMITER ',' CSV HEADER;
 
 COPY RoomBookings
-FROM 'bookings.csv'
+FROM '/extra/emira035/cs166_project/project/data/bookings.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE RoomBookings_bookingID_seq RESTART 501; 
 
 COPY RoomRepairs
-FROM 'roomRepairs.csv'
+FROM '/extra/emira035/cs166_project/project/data/roomRepairs.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairs_repairID_seq RESTART 11;
 
 COPY RoomRepairRequests
-FROM 'roomRepairRequests.csv'
+FROM '/extra/emira035/cs166_project/project/data/roomRepairRequests.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomRepairRequests_requestNumber_seq RESTART 11;
 
 COPY RoomUpdatesLog
-FROM 'roomUpdatesLog.csv'
+FROM '/extra/emira035/cs166_project/project/data/roomUpdatesLog.csv'
 WITH DELIMITER ',' CSV HEADER;
 ALTER SEQUENCE roomUpdatesLog_updateNumber_seq RESTART 51;
